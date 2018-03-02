aa38732632b7702759bc60c9fd611a55615b0258
William <willi_garn@hotmail.com>
2018-03-02 10:48:50 -0500

    Update ListenServer.cs

--- TFTP_Server/Server/Class_TFTP/ListenServer.cs
+++ TFTP_Server/Server/Class_TFTP/ListenServer.cs
@@ -17,9 +17,13 @@ namespace Server
 
         public void ListenThread()
         {
+            // Création de la chainequi va renfermer le nom du fichier a remplir
+            string fileName;
             // Création du point local et du point distant
             EndPoint LocalPoint = new IPEndPoint(0, 69);
             EndPoint DistantPoint = new IPEndPoint(0, 0);
+            // Création du tableau de byte qui va renfermer la réception
+            byte[] bReception = new byte[516];
 
             // Écoute du serveur
             try
@@ -29,11 +33,22 @@ namespace Server
                     SocketType.Dgram, ProtocolType.Udp);
                 socket.Bind(LocalPoint);
 
+                //Boucle du thread
                 while (!m_fin)
                 {
                     if (socket.Available > 0)
                     {
-
+                        socket.ReceiveFrom(bReception, ref DistantPoint);
+                        switch (TrameValidation(bReception))
+                        {
+                            case 1: // Read request
+                                fileName = null;
+                                for (int i = 2; bReception[i] != (byte)0; i++)
+                                {
+                                    fileName += (char)bReception[i];
+                                }
+                                break; // La methode n'est pas complete
+                        }
                     }
                 }
             }
@@ -41,6 +56,7 @@ namespace Server
             {
                 MessageBox.Show(se.Message, "Problème avec le Socket",
                     MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
             }
         }
 
