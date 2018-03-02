b9ab2ef1a2c9561677d74d2aa7fb20d17270a920
William <willi_garn@hotmail.com>
2018-03-01 20:36:39 -0500

    Added errormsg class
    Énumération pour les types d'erreurs tftp

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# TFTP Server
+﻿# TFTP Server
 
 Laboratoire 6 du cours de Réseau et Linux (Réseau)
 
 Cégep-Lévis-Lauzon
 
 Session 4
+
---
+++ TFTP_Server/Server/TFTP/ErrorMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.TFTP
+{
+    // Messages d'erreurs tftp, voir page 68
+    enum ErrorMessage
+    {
+        NON_DEFINI,
+        FICHIER_NON_TROUVE,
+        VIOLATION_ACCES,
+        DISQUE_PLEIN,
+        OPERATION_TFTP_ILLEGALE,
+        TRANSFERT_ID_INCONNU,
+        FICHIER_EXISTANT,
+        UTILISATEUR_INCONNU
+    };
+}
